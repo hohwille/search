91d8772829be3c7917c5cc7b4b199dab654ca91b
Joerg Hohwiller <hohwille@users.sourceforge.net>
2016-05-20 01:36:23 +0200

    further fix

--- search/engine/impl-lucene/src/test/java/net/sf/mmm/search/engine/impl/lucene/LuceneSearchQueryBuilderFactorySpringTest.java
+++ search/engine/impl-lucene/src/test/java/net/sf/mmm/search/engine/impl/lucene/LuceneSearchQueryBuilderFactorySpringTest.java
@@ -2,26 +2,32 @@
  * http://www.apache.org/licenses/LICENSE-2.0 */
 package net.sf.mmm.search.engine.impl.lucene;
 
+import javax.inject.Inject;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import net.sf.mmm.search.engine.api.SearchQueryBuilderFactory;
-import net.sf.mmm.util.component.base.SpringConfigs;
-import net.sf.mmm.util.component.impl.SpringContainerPool;
+import net.sf.mmm.search.engine.impl.spring.SearchEngineSpringConfig;
 
 /**
  * This is the test-case for {@link LuceneSearchQueryBuilderFactory} configured using spring.
- * 
+ *
  * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
  * @since 1.0.0
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { SearchEngineSpringConfig.class })
 public class LuceneSearchQueryBuilderFactorySpringTest extends LuceneSearchQueryBuilderFactoryTest {
 
-  /**
-   * {@inheritDoc}
-   */
+  @Inject
+  private SearchQueryBuilderFactory searchQueryBuilderFactory;
+
   @Override
   protected SearchQueryBuilderFactory getQueryBuilderFactory() {
 
-    return SpringContainerPool.getInstance(SpringConfigs.SPRING_XML_SEARCH).get(
-        LuceneSearchQueryBuilderFactory.class);
+    return this.searchQueryBuilderFactory;
   }
 
 }
